4ca38c68188b5fa66453ce6192ad62dd12f6ab7d
zachczx <zixian.chen@gmail.com>
2024-06-25 01:16:10 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -512,7 +512,7 @@
 										d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12 2.5c4.478 0 6.718 0 8.109 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.718 0-8.109-1.391S2.5 16.479 2.5 12m0-3h19M7 6h.009M11 6h.009" /><path
 										d="m8 13l1.227 1.057c.515.445.773.667.773.943s-.258.498-.773.943L8 17m4 0h4" /></g
 								></svg
-							>Sveltekit, Gsap
+							>Sveltekit, Gsap, Tailwind
 						</div>
 						<div>
 							<svg
@@ -571,7 +571,7 @@
 										d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12 2.5c4.478 0 6.718 0 8.109 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.718 0-8.109-1.391S2.5 16.479 2.5 12m0-3h19M7 6h.009M11 6h.009" /><path
 										d="m8 13l1.227 1.057c.515.445.773.667.773.943s-.258.498-.773.943L8 17m4 0h4" /></g
 								></svg
-							>Sveltekit, SortableJS
+							>Sveltekit, SortableJS, Tailwind
 						</div>
 						<div>
 							<svg
@@ -627,7 +627,7 @@
 										d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12 2.5c4.478 0 6.718 0 8.109 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.718 0-8.109-1.391S2.5 16.479 2.5 12m0-3h19M7 6h.009M11 6h.009" /><path
 										d="m8 13l1.227 1.057c.515.445.773.667.773.943s-.258.498-.773.943L8 17m4 0h4" /></g
 								></svg
-							>Django, HTMX, SortableJS
+							>Django, HTMX, SortableJS, Bootstrap
 						</div>
 						<div>
 							<svg
@@ -683,7 +683,7 @@
 										d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12 2.5c4.478 0 6.718 0 8.109 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.718 0-8.109-1.391S2.5 16.479 2.5 12m0-3h19M7 6h.009M11 6h.009" /><path
 										d="m8 13l1.227 1.057c.515.445.773.667.773.943s-.258.498-.773.943L8 17m4 0h4" /></g
 								></svg
-							>Django, HTMX
+							>Django, HTMX, Bootstrap
 						</div>
 						<div>
 							<svg
@@ -804,7 +804,7 @@
 											d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12 2.5c4.478 0 6.718 0 8.109 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.718 0-8.109-1.391S2.5 16.479 2.5 12m0-3h19M7 6h.009M11 6h.009" /><path
 											d="m8 13l1.227 1.057c.515.445.773.667.773.943s-.258.498-.773.943L8 17m4 0h4" /></g
 									></svg
-								>AstroJS
+								>AstroJS, Tailwind, Pagefind
 							</div>
 							<div>
 								<svg
@@ -829,9 +829,8 @@
 
 						<p class="px-2">
 							Migrated <span class="font-bold text-primary underline hover:text-secondary"
-								><a href="https://btonomics.com/">BTOnomics</a></span> to AstroJS, in part to see if it lives up to the hype.
-							Not disappointed. I used ChatGPT to convert WordPress html/shortcode to markdown, which was smooth after a
-							few tries.
+								><a href="https://btonomics.com/">BTOnomics</a></span> to AstroJS, in part to try it. Enjoyed it. I used
+							ChatGPT to convert WordPress html/shortcode to markdown, which was smooth after a few tries.
 						</p>
 
 						<p class="px-2">
