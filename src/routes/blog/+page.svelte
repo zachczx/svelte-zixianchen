f8bdbfec4044c7d736812431a261a982c3a1ff39
zachczx <zixian.chen@gmail.com>
2024-06-30 10:35:28 +0800

    Update +page.svelte

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,23 @@
 <script>
 	let { data } = $props();
-	console.log(data);
+
+	/**
+	 * @function changeDateFormat - change dates from dd-mm-yyy to dd mmm yyy
+	 * @param {Date} date - from .md file in date format
+	 */
+	function changeDateFormat(date) {
+		let objectDate = new Date(date);
+
+		let day = objectDate.getDate();
+
+		let month = objectDate.getMonth();
+		const monthsList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+		const adjustedMonth = monthsList[month];
+
+		let year = objectDate.getFullYear();
+
+		return `${day} ${adjustedMonth} ${year}`;
+	}
 </script>
 
 <svelte:head><title>Zixian's Blog</title></svelte:head>
@@ -10,7 +27,7 @@
 			<h2 class="text-2xl text-green-600 underline hover:text-orange-600">
 				<a href="/blog/{post.slug}">{post.title}</a>
 			</h2>
-			<div>{post.date}</div>
+			<div>{changeDateFormat(post.date)}</div>
 		</div>
 	{/each}
 </div>
