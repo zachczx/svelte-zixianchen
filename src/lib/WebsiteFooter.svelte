ae6a3b92e4f8c52a93679193dde29ce1f8cae2f5
zachczx <zixian.chen@gmail.com>
2024-07-01 23:30:23 +0800

    Create WebsiteFooter.svelte

---
+++ src/lib/WebsiteFooter.svelte
@@ -0,0 +1,7 @@
+<div>
+	<p class="mt-24 text-center text-base">Copyright © 2024 Zixian Chen. All rights reserved.</p>
+	<p class="text-center text-base">
+		Communication illustrations by <a href="https://www.freepik.com/">Freepik</a> and
+		<a href="https://storyset.com/communication">Storyset</a>.
+	</p>
+</div>
