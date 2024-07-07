90ac58cd3e81f586edadf33aa70c3de4cae76062
zachczx <zixian.chen@gmail.com>
2024-07-07 18:33:55 +0800

    Fixed overflow issues

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -4,12 +4,12 @@
 </script>
 
 <svelte:head><title>{data.metadata.title} | Zixian's Blog</title></svelte:head>
-<h1 class="text-2xl xl:text-4xl">{data.metadata.title}</h1>
-<div class="text-medium">{changeDateFormat(data.metadata.date)}</div>
+<h1 class="px-3 text-2xl xl:text-4xl">{data.metadata.title}</h1>
+<div class="text-medium px-3">{changeDateFormat(data.metadata.date)}</div>
 <article
-	class="prose:max-w-sm prose prose-sm mt-6 prose-a:font-semibold prose-a:text-orange-600 prose-a:decoration-2 prose-a:underline-offset-2 hover:prose-a:text-orange-400 prose-code:font-['Fira_Code_Variable'] lg:mt-10 lg:max-w-none">
+	class="prose:max-w-sm prose prose-sm mt-6 w-screen px-3 prose-a:font-semibold prose-a:text-orange-600 prose-a:decoration-2 prose-a:underline-offset-2 hover:prose-a:text-orange-400 prose-code:font-['Fira_Code_Variable'] lg:mt-10 lg:max-w-none">
 	<svelte:component this={data.content} />
 </article>
-<div class="pt-10 text-gray-600 lg:pt-20">
+<div class="px-3 pt-10 text-gray-600 lg:pt-20">
 	Tags: {#each data.metadata.tags as tag}<span class="me-2 border border-neutral px-2 py-1">{tag}</span>{/each}
 </div>
