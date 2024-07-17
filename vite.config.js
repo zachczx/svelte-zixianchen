0a212017bac0a0bf120d9733771c70b754d25f13
zachczx <zixian.chen@gmail.com>
2024-07-18 00:10:00 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import svg from '@poppanator/sveltekit-svg';
 import { defineConfig } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 
