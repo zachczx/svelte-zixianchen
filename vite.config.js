a24472bf678f0e90948701a0c41d6037747cd62d
zachczx <zixian.chen@gmail.com>
2024-07-18 00:06:11 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,20 +8,6 @@ export default defineConfig({
 	plugins: [
 		enhancedImages(),
 		sveltekit(),
-		svg({
-			includePaths: ['./src/lib/icons/', './static/'],
-			svgoOptions: {
-				multipass: true,
-				plugins: [
-					{
-						name: 'preset-default',
-						// by default svgo removes the viewBox which prevents svg icons from scaling
-						// not a good idea! https://github.com/svg/svgo/pull/1461
-						params: { overrides: { removeViewBox: false } },
-					},
-				],
-			},
-		}),
 		visualizer({
 			emitFile: true,
 			filename: 'stats.html',
