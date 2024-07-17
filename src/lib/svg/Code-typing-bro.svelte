44a395eedbdcfeb86bbc78291f83d0dcc513d03e
zachczx <zixian.chen@gmail.com>
2024-07-17 22:15:25 +0800

    Try switching svelte to svg

--- src/lib/svg/Code-typing-bro.svelte
+++ src/lib/svg/Code-typing-bro.svelte
@@ -5,7 +5,6 @@
 	xmlns="http://www.w3.org/2000/svg"
 	xmlns:xlink="http://www.w3.org/1999/xlink"
 	xml:space="preserve"
-	xmlns:serif="http://www.serif.com/"
 	style="fill-rule:evenodd;clip-rule:evenodd;stroke-miterlimit:10;">
 	<g id="freepik--background-simple--inject-135">
 		<path
