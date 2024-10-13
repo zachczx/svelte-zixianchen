9227b4cf6ec19009faeaf631655ead9066217da7
zachczx <zixian.chen@gmail.com>
2024-10-13 11:54:16 +0800

    Update +page.js

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -9,9 +9,9 @@ import { redirect } from '@sveltejs/kit';
  * @returns {JSON} - post.default is .md content (needs to be accessed by <svelte:component this={data.content} />), post.metadata is .md frontmatter
  */
 export async function load({ params }) {
-	if (params.slug === 'using-brevo-pocketbase-send-emails') {
-		redirect(301, '/blog/using-resend-pocketbase-send-emails');
-	}
+	// if (params.slug === 'using-brevo-pocketbase-send-emails') {
+	// 	redirect(301, '/blog/using-resend-pocketbase-send-emails');
+	// }
 
 	const post = await import(`../posts/${params.slug}.md`);
 	return { content: post.default, metadata: post.metadata };
