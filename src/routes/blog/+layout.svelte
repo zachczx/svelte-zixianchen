cfcc66448b2813ae14ae46f72548363372d82cc2
zachczx <zixian.chen@gmail.com>
2024-10-13 22:25:46 +0800

    Update +layout.svelte
    Adjusted ghost shifting animation to use getElementById in mouseover callback instead of svelte ternary

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -2,6 +2,8 @@
 	import Particles from '$lib/magic-ui/Particles.svelte';
 
 	let ghostColor = $state('');
+	const ghostAddClassMainRight = ['translate-x-4', 'text-red-900'];
+	const ghostAddClassBlogLeft = ['-translate-x-4', 'text-orange-400'];
 </script>
 
 <svelte:head>
@@ -27,48 +29,54 @@
 				<div class="view-blog-nav border-2 border-orange-400">
 					<a
 						href="/blog"
-						class="view-blog-blog inline-block min-w-24 border-r-2 border-r-orange-400 bg-orange-100 px-4 py-2 font-bold"
+						class="view-blog-blog inline-block min-w-24 border-r-2 border-r-orange-400 bg-orange-100 px-4 py-2 font-bold hover:text-orange-600"
 						onmouseover={() => {
-							ghostColor = 'orange';
+							const ghostEl = document.getElementById('ghost');
+							ghostEl.classList.add(...ghostAddClassBlogLeft);
 						}}
 						onfocus={() => {
-							ghostColor = 'orange';
+							const ghostEl = document.getElementById('ghost');
+							ghostEl.classList.add(...ghostAddClassBlogLeft);
 						}}
 						onmouseout={() => {
-							ghostColor = '';
+							const ghostEl = document.getElementById('ghost');
+							ghostEl.classList.remove(...ghostAddClassBlogLeft);
 						}}
 						onblur={() => {
-							ghostColor = '';
+							const ghostEl = document.getElementById('ghost');
+							ghostEl.classList.remove(...ghostAddClassBlogLeft);
 						}}>Blog</a>
 					{#key ghostColor}<span class="inline-block min-w-20"
 							><svg
+								id="ghost"
 								xmlns="http://www.w3.org/2000/svg"
 								width="2em"
-								fill={ghostColor ? ghostColor : 'currentColor'}
+								fill="currentColor"
 								height="2em"
 								viewBox="0 0 24 24"
-								class="icon icon-tabler icons-tabler-filled icon-tabler-ghost inline {ghostColor === 'red'
-									? 'animate-right'
-									: ghostColor === 'orange'
-										? 'animate-left'
-										: undefined}"
+								class="icon icon-tabler icons-tabler-filled icon-tabler-ghost inline transition duration-500"
 								><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
 									d="M12 3a8 8 0 0 1 7.996 7.75l.004 .25l-.001 6.954l.01 .103a2.78 2.78 0 0 1 -1.468 2.618l-.163 .08c-1.053 .475 -2.283 .248 -3.129 -.593l-.137 -.146a.65 .65 0 0 0 -1.024 0a2.65 2.65 0 0 1 -4.176 0a.65 .65 0 0 0 -.512 -.25c-.2 0 -.389 .092 -.55 .296a2.78 2.78 0 0 1 -4.859 -2.005l.008 -.091l.001 -6.966l.004 -.25a8 8 0 0 1 7.996 -7.75zm2.82 10.429a1 1 0 0 0 -1.391 -.25a2.5 2.5 0 0 1 -2.858 0a1 1 0 0 0 -1.142 1.642a4.5 4.5 0 0 0 5.142 0a1 1 0 0 0 .25 -1.392zm-4.81 -4.429l-.127 .007a1 1 0 0 0 .117 1.993l.127 -.007a1 1 0 0 0 -.117 -1.993zm4 0l-.127 .007a1 1 0 0 0 .117 1.993l.127 -.007a1 1 0 0 0 -.117 -1.993z" /></svg>
 						</span>{/key}
 					<a
 						href="/"
-						class="view-blog-home inline-block min-w-24 border-l-2 border-l-orange-400 bg-orange-100 px-4 py-2 font-bold"
+						class="view-blog-home inline-block min-w-24 border-l-2 border-l-orange-400 bg-orange-100 px-4 py-2 font-bold hover:text-red-900"
 						onmouseover={() => {
-							ghostColor = 'red';
+							const ghostEl = document.getElementById('ghost');
+							ghostEl.classList.add(...ghostAddClassMainRight);
 						}}
 						onfocus={() => {
-							ghostColor = 'red';
+							const ghostEl = document.getElementById('ghost');
+							ghostEl.classList.add(...ghostAddClassMainRight);
 						}}
 						onmouseout={() => {
-							ghostColor = '';
+							// ghostColor = '';
+							const ghostEl = document.getElementById('ghost');
+							ghostEl.classList.remove(...ghostAddClassMainRight);
 						}}
 						onblur={() => {
-							ghostColor = '';
+							const ghostEl = document.getElementById('ghost');
+							ghostEl.classList.remove(...ghostAddClassMainRight);
 						}}>Main</a>
 				</div>
 			</div>
