84bb2d6dcbef4333a03afc3b5675eaaccec2499a
zachczx <zixian.chen@gmail.com>
2025-02-20 22:26:36 +0800

    Update +page.svelte
    Minor update to remove deprecated way of using svelte:component

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	let { data } = $props();
 	import changeDateFormat from '$lib/BlogDate';
+	let content = $state(data.content);
 </script>
 
 <svelte:head>
@@ -18,7 +19,10 @@
 </div>
 <article
 	class="prose mt-6 w-screen px-3 prose-a:font-semibold prose-a:text-orange-600 prose-a:decoration-2 prose-a:underline-offset-2 hover:prose-a:text-orange-400 prose-blockquote:my-8 prose-blockquote:ms-8 prose-code:font-['Fira_Code_Variable'] prose-pre:p-0 sm:px-6 md:max-w-none lg:mt-10 lg:max-w-[1024px] xl:w-[1024px] xl:px-14">
-	<svelte:component this={data.content} />
+	{#if content}
+		{@const Component = content}
+		<Component />
+	{/if}
 </article>
 <div class="px-3 pt-10 text-gray-600 sm:px-6 lg:pt-20 xl:px-14">
 	Tags: {#each data.metadata.tags as tag}<span class="me-2 border border-neutral px-2 py-1">{tag}</span>{/each}
