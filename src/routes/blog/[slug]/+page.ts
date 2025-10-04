0f80d60c344cd7f8a30eaed7abb49629b3fd2a28
zachczx <zixian.chen@gmail.com>
2025-10-04 15:41:12 +0800

    Update +page.ts

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,10 +1,16 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
+import type { Component } from 'svelte';
+
+interface MdsvexModule {
+	default: Component;
+	metadata: BlogPostMetadata;
+}
 
 export const load: PageLoad = async ({ params }) => {
 	const slug = params.slug;
 
-	const posts = import.meta.glob('../posts/*.md', { eager: true });
+	const posts = import.meta.glob<MdsvexModule>('../posts/*.md', { eager: true });
 	const postPath = `../posts/${slug}.md`;
 	if (!(postPath in posts)) error(404, 'post not found!');
 
