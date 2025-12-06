8754e5fb164ef8dcb21d189838b59cf3bca318fb
zachczx <zixian.chen@gmail.com>
2025-12-06 09:53:24 +0800

    fix: add view transition js activation
    Already auto in css, but requires js onNavigate hook

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script>
-	let { children } = $props();
+	import { onNavigate } from '$app/navigation';
 	import '../app.css';
+
+	let { children } = $props();
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 {@render children()}
