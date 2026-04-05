75170e6e0f129a244c01cd7ccbeb13409eab9a77
zachczx <zixian.chen@gmail.com>
2026-04-05 09:37:46 +0800

    refactor: migrate Nav from $app/stores to $app/state

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import NavDock from '$lib/NavDock.svelte';
 
 	let { navCurrent }: { navCurrent: string } = $props();
@@ -9,7 +9,7 @@
 	<div
 		style="view-transition-name: navdock;"
 		class="hidden h-20 w-fit min-w-120 items-center justify-center justify-self-center rounded border border-gray-400 bg-gray-900 px-2 shadow-sm backdrop-blur-md transition-all duration-300 ease-out xl:relative xl:flex">
-		<NavDock {navCurrent} pathName={$page.url.pathname} />
+		<NavDock {navCurrent} pathName={page.url.pathname} />
 	</div>
 	<div class="hidden items-center justify-end space-x-6 xl:flex"></div>
 </nav>
