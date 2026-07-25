a46d0816ab5d3a4d20a22dd7573ddb0a0255b0ec
zachczx <109778851+zachczx@users.noreply.github.com>
2026-07-25 12:38:18 +0800

    feat: adjusted cv wording to align with official titles

--- src/routes/jobs.ts
+++ src/routes/jobs.ts
@@ -1,32 +1,32 @@
 export const jobs = [
 	{
 		year: '2023',
-		title: 'Service Delivery Tech Team Lead',
+		title: 'Deputy Director, Service Delivery Digitalization',
 		desc: "Build, run stuff for citizens + employees services. Also dismantle what doesn't work.",
 	},
 	{
 		year: '2021',
-		title: 'Tech Infra Policy Team Lead',
+		title: 'Deputy Director, Policy & Governance Directorate',
 		desc: 'Policies & funding for Govt cloud, on-prem hosting, SG Tech Stack, endpoint devices, dev toolchains.',
 	},
 	{
 		year: '2018',
-		title: 'Comms Tech Team Lead',
+		title: 'Assistant Director, Communications Technology',
 		desc: 'Developed & bought media analytic products. Led investments in NLP & CV research.',
 	},
 	{
 		year: '2017',
-		title: 'Comms Strategist',
-		desc: 'Did comms campaigns and strategies.',
+		title: 'Communications Strategist',
+		desc: 'Developed comms campaigns and strategies.',
 	},
 	{
 		year: '2015',
 		title: 'Media Relations Officer',
-		desc: 'Did public relations work. Got earned media. Did crisis comms.',
+		desc: 'Did public relations work. Got earned media. Managed crisis comms.',
 	},
 	{
 		year: '2013',
-		title: 'NS Policy Officer',
-		desc: 'Did NS policies for sportsmen, leave, citizenship, exit control.',
+		title: 'NS Policy Executive',
+		desc: 'Led NS policies for sportsmen, leave, citizenship, exit control.',
 	},
 ];
