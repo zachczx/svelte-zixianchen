b60384dea243ba346caae4104e032394ec09c608
Zixian <109778851+zachczx@users.noreply.github.com>
2026-08-08 17:46:11 +0800

    fix: clarify blog RSS title

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -45,7 +45,7 @@ export const GET: RequestHandler = () => {
 	const xml = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 \t<channel>
-\t\t<title>Zixian Chen</title>
+\t\t<title>Zixian Chen - Blog</title>
 \t\t<link>${origin}/blog</link>
 \t\t<description>Blog posts by Zixian Chen.</description>
 \t\t<language>en</language>
